f8e4bef9f3e24a3ee22bd664d2965e2f632e2c18
Lorena Muralha <mlmuralha2301@gmail.com>
2024-08-23 08:52:05 -0300

    fix: fixed consumer JSON conversion issue

---
+++ README.md
@@ -0,0 +1,23 @@
+# RabbitMQ API RESTful e Microsserviços
+
+## Sobre o projeto👾
+
+Este projeto é um sistema de um microserviço reponsável por receber requisições e gerar mensagens para os demais microsserviços, que são os consumidores da mensagem.
+
+## Objetivo🎯
+
+Esse projeto visa aprimorar o aprendizado sobre o uso de Java com Spring Framework, Message Brokers, APIRESTful e criação de microserviços, além da integração de todos esses componentes juntos. Embora um projeto simples, feedbacks sobre código, estrutura ou qualquer aspecto relacionado que possa contribuir para melhorias serão bem-vindos e valorizados.
+
+## Instruções de Instalação e Execução do Projeto 🛠️
+
+### Requisitos Necessários:
+
+- Java 17.0.10 ou superior.
+- Docker 27.1.1
+- IDE que execute Java (NetBeans, IntelliJ, Eclipse, por exemplo).
+-
+
+```bash
+  npm install my-project
+  cd my-project
+```
--- consumer/src/main/java/com/mycompany/consumer/consumers/InventoryConsumer.java
+++ consumer/src/main/java/com/mycompany/consumer/consumers/InventoryConsumer.java
@@ -8,16 +8,25 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class InventoryConsumer {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @RabbitListener(queues = RabbitMQConstants.QUEUE_INVENTORY)
-    private void consumer(Message message) {
-        MessageConverter converter = new Jackson2JsonMessageConverter();
-        InventoryDTO inventoryDTO = (InventoryDTO) converter.fromMessage(message);
-        System.out.println("Product Code: " + inventoryDTO.productCode);
-        System.out.println("Quantity: " + inventoryDTO.quantity);
+    private void consumer(String message) {
+        try {
+            // Converte a String JSON para o objeto InventoryDTO
+            InventoryDTO inventoryDTO = objectMapper.readValue(message, InventoryDTO.class);
+            System.out.println("Product Code: " + inventoryDTO.productCode);
+            System.out.println("Quantity: " + inventoryDTO.quantity);
+        } catch (Exception e) {
+            System.out.println("-----ERROR DEBUG-----");
+            e.printStackTrace();
+            System.out.println("----------------------");
+        }
     }
 }
